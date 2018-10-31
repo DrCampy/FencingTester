6b69a379e2016346f025404a56e53939257964b9
Morgan Diepart <morgandiepart@gmail.com>
2018-10-31 21:53:44 +0100

    i2C

--- Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.cpp
+++ Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.cpp
@@ -1,62 +1,157 @@
 ﻿#include "Bargraph.h"
+#include <util/twi.h>
+#include <limits.h>
 
-void Bargraph::setValue(short int value){
+#define RED_PWM_RATIO 0.5
+#define GRN_PWN_RATIO 0.5
+#define ORG_PWM_RATIO 1.0
+
+#define CHAR_RED_PWM (unsigned char)UCHAR_MAX*RED_PWM_RATIO
+#define CHAR_GRN_PWM (unsigned char)UCHAR_MAX*GRN_PWN_RATIO
+#define CHAR_ORG_PWM (unsigned char)UCHAR_MAX*ORG_PWM_RATIO
+
+#define SLA_R 0b1110001
+#define SLA_W 0b1110000
+
+
+
+void Bargraph::setValue(short unsigned int value){
 		/* Checks that value is in the input range*/
-		if(value == 0){ //If we want to display an empty bgraph we need to otput to an unused pin, done with value = 10
-			value = 11;
-		}else if(value > 10){
+		if(value > 10){
 			value = 10;
 		}
 		
 		lastValue = value;
+		
 		/*If we need to display the value, processes it*/
 		if(isOn){
-			value--;
-			/*S3*/
-			if(value >> 3){
-				PORTD |= (1 << PORTD5);
-			}else{
-				PORTD &= ~(1 << PORTD5);
-			}
-			/*S2*/
-			if(value%8 >> 2){
-				PORTD |= (1 << PORTD4);
-			}else{
-				PORTD &= ~(1 << PORTD4);
-			}
-			/*S1*/
-			if(value%4 >> 1){
-				PORTD |= (1 << PORTD3);
-			}else{
-				PORTD &= ~(1 << PORTD3);
-			}
-			/*S0*/
-			if(value%2){
-				PORTD |= (1 << PORTD2);
-			}else{
-				PORTD &= ~(1 << PORTD2);
-			}
-				
+			
 		}
 	}
 		
 void Bargraph::display(bool display){
 	isOn = display;
-	if(!isOn){ //if the bargraph is off we keep it's value at 0 so it doesn't light up
+	
+	/*if the bargraph is off we keep it's value at 0 so it doesn't light up*/
+	if(!isOn){ 
 		setValue(0);
 	}else{
 		setValue(lastValue);
 	}
 }
+
 		
 Bargraph::Bargraph(){
-	/* Sets the output pins as outputs*/
-	DDRD |= (1 << PORTD2) | (1 << PORTD3) | (1 << PORTD4) | (1 << PORTD5);
-			
-	/*Ensures that their logical level is LOW*/
-	PORTD &= ~(1 << PORTD2) & ~(1 << PORTD3) & ~(1 << PORTD4) & ~(1 << PORTD5);		
+	
+
 			
 	display(true);
 	setValue(0);
 }
+
+void Bargraph::bargraphError(){
+	//Do something;
 	
+}
+
+bool Bargraph::bargraphInit(){
+		//Enables TWI
+		TWCR |= (1 << TWEN);
+		
+		//TODO what I2C clock divider/prescaler ?
+		
+		//MODE registers are kept at default values.
+		if (!sendStart()){ //Starts communication
+			bargraphError();
+		}
+		
+		if(!sendSLA_W()){ //Sends the slave adress with writing bit
+			bargraphError();
+		}
+		
+		if(!sendData(0b10000010)){ //sends the control register's value for the device (100 for auto increment then 00010 for register PWM0)
+			bargraphError();
+		}
+		
+		for(int i = 0; i < 5; i++){ //First 6 PWM registers are green values
+			if(!sendData(CHAR_GRN_PWM)){
+				bargraphError();
+			}
+		}
+
+		if(!sendData(CHAR_ORG_PWM)){ //next register is for the ORG led.
+			bargraphError();
+		}
+		
+		for(int i = 0; i < 2; i++){ //Next 3 registers are for the red LEDs
+			if(!sendData(CHAR_RED_PWM)){
+				bargraphError();
+			}
+		}
+		
+		if(!sendRStart()){
+			bargraphError();
+		}
+		
+		if(!sendSLA_W()){
+			bargraphError();
+		}
+		
+		if(!sendData(0b00011100)){
+			bargraphError();
+		}
+		
+		if(!sendData(0b/*IREF_DATAS*/)){ //TODO What to put in IREF
+			bargraphError()
+		}
+		if(!sendStop()){
+			bargraphError();
+		}
+
+
+}
+
+bool sendStart(){
+	TWCR |= (1 << TWINT) | (1 << TWSTA); //Sends start condition
+	while(!(TWCR & (1 << TWINT))); // waits for the TWINT flag set to check if start properly sent
+	if (TW_STATUS != TW_START){
+		return false;
+	}
+	return true;
+}
+
+bool sendRStart(){
+	TWCR |= (1 << TWINT) | (1 << TWSTA); //Sends start condition
+	while(!(TWCR & (1 << TWINT))); // waits for the TWINT flag set to check if start properly sent
+	if (TW_STATUS != TW_REP_START){
+		return false;
+	}
+	return true;
+}
+
+bool sendStop(){
+	TWCR |= (1 << TWINT) | (1 << TWSTO); //Sends start condition
+	return true;
+}
+
+bool sendSLA_W(){
+	TWDR = SLA_W; //Loads the adress to write to slave device
+	TWCR |= (1 << TWINT); //Tells TWI to proceed
+			
+	while(!(TWCR & (1 << TWINT))); // waits for the TWINT flag set to check if SLA_W properly sent
+	if(TW_STATUS != TW_MT_SLA_ACK){ //Check that slave acknowledges it's adress
+		return false;
+	}
+}
+
+
+bool sendData(uint8_t data){
+	TWDR = data; //sets the Two Wire data register to the data we want to send
+	
+	TWCR |= (1 << TWINT); //Tells TWI to proceed
+			
+	while(!(TWCR & (1 << TWINT))); // waits for the TWINT flag set to check if data was properly sent
+	if(TW_STATUS != TW_MT_SLA_ACK){ //Check that slave acknowledges the data
+		return false;
+	}
+}
--- Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.h
+++ Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.h
@@ -7,13 +7,13 @@
 		private:
 			bool isOn;
 			unsigned char lastValue;
-	
+			void bargraphError();
+			bool bargraphInit();
 		public:
 	
 			void setValue(short int value);
-	
 			void display(bool display); //sets the bargraph on or off
-	
+				
 			Bargraph();
 	};
 
