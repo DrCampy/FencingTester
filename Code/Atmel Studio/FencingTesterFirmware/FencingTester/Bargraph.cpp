a18bc69888a2c16048a69e08e27fb76694c9fc2f
Morgan Diepart <morgandiepart@gmail.com>
2018-11-07 17:14:28 +0100

    Bargraph::setValue coding using I2C

--- Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.cpp
+++ Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.cpp
@@ -13,7 +13,11 @@
 #define SLA_R 0b1110001
 #define SLA_W 0b1110000
 
-
+bool sendStart();
+bool sendRStart();
+bool sendStop();
+bool sendSLA_W();
+bool sendData(uint8_t data);
 
 void Bargraph::setValue(short unsigned int value){
 		/* Checks that value is in the input range*/
@@ -21,14 +25,69 @@ void Bargraph::setValue(short unsigned int value){
 			value = 10;
 		}
 		
+		if(value == lastValue){
+			return;
+		}
+		
+		unsigned char LEDOUT0 = 0, LEDOUT1 = 0, LEDOUT2 = 0;
+		for(unsigned char i = 1; i < 4; i++){
+			LEDOUT0 <<= 2;
+			if(i <= value){ //This led should be on
+				LEDOUT0 += 0b10;
+			}
+			//This led should be off, do nothing
+		}		
+		for(unsigned char i = 5; i < 8; i++){
+			LEDOUT1 <<= 2;
+			if(i <= value){ //This led should be on
+				LEDOUT1 += 0b10;
+			}
+			//This led should be off, do nothing
+		}		
+		for(unsigned char i = 9; i < 10; i++){
+			LEDOUT2 <<= 2;
+			if(i <= value){ //This led should be on
+				LEDOUT2 += 0b10;
+			}
+			//This led should be off, do nothing
+		}
+		if(!sendStart()){
+			bargraphError();
+		}
+		
+		//Check which registers need to be sent
+		//register 14h
+		if(prevLEDOUT0 ^ LEDOUT0){
+			if(!sendData(0b00010100)){
+				bargraphError();
+			}
+			if(!sendData(LEDOUT0)){
+				bargraphError();
+			}
+		}
+		
+		//register 15h
+		if(prevLEDOUT1 ^ LEDOUT1){
+			if(!sendData(0b00010101)){
+				bargraphError();
+			}
+			if(!sendData(LEDOUT1)){
+				bargraphError();
+			}
+		}
+		//register 16h
+		if(prevLEDOUT2 ^ LEDOUT2){
+			if(!sendData(0b00010110)){
+				bargraphError();
+			}
+			if(!sendData(LEDOUT2)){
+				bargraphError();
+			}
+		}
 		
 		/*If we need to display the value, processes it*/
 		if(isOn){
-			if(lastValue <= 4){
-
-			}else if(lastValue <= 8){
-				
-			}
+			
 		}
 		lastValue = value;
 	}
@@ -60,63 +119,63 @@ void Bargraph::bargraphError(){
 }
 
 bool Bargraph::bargraphInit(){
-		//Enables TWI
-		TWCR |= (1 << TWEN);
+	//Enables TWI
+	TWCR |= (1 << TWEN);
 		
-		//TODO what I2C clock divider/prescaler ?
+	//TODO what I2C clock divider/prescaler ?
 		
-		//MODE registers are kept at default values.
-		if (!sendStart()){ //Starts communication
+	//MODE registers are kept at default values.
+	if (!sendStart()){ //Starts communication
+		return false;
+	}
+		
+	if(!sendSLA_W()){ //Sends the slave adress with writing bit
+		return false;
+	}
+		
+	if(!sendData(0b10000010)){ //sends the control register's value for the device (100 for auto increment then 00010 for register PWM0)
+		return false;
+	}
+		
+	for(int i = 0; i < 5; i++){ //First 6 PWM registers are green values
+		if(!sendData(CHAR_GRN_PWM)){
 			return false;
 		}
-		
-		if(!sendSLA_W()){ //Sends the slave adress with writing bit
-			return false;
-		}
-		
-		if(!sendData(0b10000010)){ //sends the control register's value for the device (100 for auto increment then 00010 for register PWM0)
-			return false;
-		}
-		
-		for(int i = 0; i < 5; i++){ //First 6 PWM registers are green values
-			if(!sendData(CHAR_GRN_PWM)){
-				return false;
-			}
-		}
+	}
 
-		if(!sendData(CHAR_ORG_PWM)){ //next register is for the ORG led.
-			return false;
+	if(!sendData(CHAR_ORG_PWM)){ //next register is for the ORG led.
+		return false;
 
-		}
+	}
 		
-		for(int i = 0; i < 2; i++){ //Next 3 registers are for the red LEDs
-			if(!sendData(CHAR_RED_PWM)){
-				return false;
-			}
-		}
-		
-		if(!sendRStart()){
+	for(int i = 0; i < 2; i++){ //Next 3 registers are for the red LEDs
+		if(!sendData(CHAR_RED_PWM)){
 			return false;
 		}
+	}
 		
-		if(!sendSLA_W()){
-			return false;
-		}
+	if(!sendRStart()){
+		return false;
+	}
 		
-		//1C
-		if(!sendData(0b00011100)){
-			return false;
-		}
+	if(!sendSLA_W()){
+		return false;
+	}
 		
-		//IREF : see associated excel sheet
-		if(!sendData(0b01111001)){ //TODO What to put in IREF
-			return false;
-		}
-		if(!sendStop()){
-			return false;
-		}
+	//1C
+	if(!sendData(0b00011100)){
+		return false;
+	}
+		
+	//IREF : see associated excel sheet
+	if(!sendData(0b01111001)){ //TODO What to put in IREF
+		return false;
+	}
+	if(!sendStop()){
+		return false;
+	}
 
-
+	return true;
 }
 
 bool sendStart(){
@@ -150,6 +209,7 @@ bool sendSLA_W(){
 	if(TW_STATUS != TW_MT_SLA_ACK){ //Check that slave acknowledges it's adress
 		return false;
 	}
+	return true;
 }
 
 
@@ -162,4 +222,5 @@ bool sendData(uint8_t data){
 	if(TW_STATUS != TW_MT_SLA_ACK){ //Check that slave acknowledges the data
 		return false;
 	}
+	return true;
 }
--- Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.h
+++ Code/Atmel Studio/FencingTesterFirmware/FencingTester/Bargraph.h
@@ -7,11 +7,12 @@
 		private:
 			bool isOn;
 			unsigned char lastValue;
+			unsigned char prevLEDOUT0, prevLEDOUT1, prevLEDOUT2;
 			void bargraphError();
 			bool bargraphInit();
 		public:
 	
-			void setValue(short int value);
+			void setValue(short unsigned int value);
 			void display(bool display); //sets the bargraph on or off
 				
 			Bargraph();
